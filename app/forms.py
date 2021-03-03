235fdecea06791c76eea6db8b6bdba444fa6a83b
Brian <bstanton773@gmail.com>
2021-03-03 16:45:55 -0600

    Wednesday afternoon

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired, EqualTo, Email
 
 
@@ -14,4 +14,11 @@ class UserInfoForm(FlaskForm):
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField()
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField()
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
-from app import app, db
+from app import app, db, mail, Message
 from flask import render_template, request, flash, redirect, url_for
-from app.forms import UserInfoForm, PostForm
+from app.forms import UserInfoForm, PostForm, LoginForm
 from app.models import User
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import check_password_hash
 
 @app.route('/')
 @app.route('/index')
-def hello_world():
+def index():
     context = {
         'title': 'Kekambas Blog | HOME',
         'customer_name': 'Brian',
@@ -31,12 +33,20 @@ def register():
         # commit database
         db.session.commit()
 
+        # Send email to new user
+        msg = Message(f'Welcome, {username}', [email])
+        msg.body = "Thank you for signing up for the Kekambas blog. I hope you enjoy our app!"
+        msg.html = "<p>Thank you so much for signing up for the Kekambas blog. I hope you enjoy our app!</p>"
+
+        mail.send(msg)
+
         flash("You have succesfully signed up!", "success")
-        return redirect(url_for('hello_world'))
+        return redirect(url_for('index'))
     return render_template('register.html', title=title, form=form)
 
 
 @app.route('/createpost', methods=['GET', 'POST'])
+@login_required
 def createpost():
     title = "Kekambas Blog | CREATE POST"
     post = PostForm()
@@ -45,3 +55,34 @@ def createpost():
         content = post.content.data
         print(post_title, content)
     return render_template('create_post.html', post=post, title=title)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    title = "Kekambas Blog | LOGIN"
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username).first()
+
+        if user is None or not check_password_hash(user.password, password):
+            flash("Incorrect Email/Password. Please try again", 'danger')
+            return redirect(url_for('login'))
+        
+        login_user(user, remember=form.remember_me.data)
+        flash("You have successfully logged in!", 'success')3
+        next_page = request.args.get('next')
+        if next_page:
+            return redirect(url_for(next_page.lstrip('/')))
+        return redirect(url_for('index'))
+
+    return render_template('login.html', title=title, form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("You have succesfully logged out", 'primary')
+    return redirect(url_for('index'))
